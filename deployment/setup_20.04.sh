d8d6937d11ef7bf6a99e5a3354b9ca78420a9eaa
Shivam Kumar <csautomaytic@gmail.com>
2023-12-11 18:55:14 +0530

    rebranded

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -467,7 +467,7 @@ function cwctl_message() {
 #   None
 ##############################################################################
 function get_cw_version() {
-  CW_VERSION=$(curl -s https://app.chatwoot.com/api | python3 -c 'import sys,json;data=json.loads(sys.stdin.read()); print(data["version"])')
+  CW_VERSION=$(curl -s https://info.holboxphotos.com/api | python3 -c 'import sys,json;data=json.loads(sys.stdin.read()); print(data["version"])')
 }
 
 ##############################################################################
